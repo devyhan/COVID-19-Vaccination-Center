39f663b559ab26b1b83bbd2eeeccf5d3bc101bcd
devyhan <devyhan93@gmail.com>
2022-08-24 13:15:00 +0900

    Initial Commit

---
+++ COVID-19-vaccination-center/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19-vaccination-center
+//
+//  Created by YHAN on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
